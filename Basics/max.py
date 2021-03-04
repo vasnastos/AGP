63b19d81b2dfc2d7e5a90311dc4bd8db2ac19da5
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2021-03-04 10:52:59 +0200

    Add files via upload

--- Basics/max.py
+++ Basics/max.py
@@ -1,4 +1,6 @@
-words=['Hello','world','Test','Logging','Simplefied','Auto']
+words=['Hello','world','Test','Logging','Simplefied','Auto','Satisfied_']
 x=max(words,key=len)
 y=min(words,key=len)
 print(x,'-',y)
+l=[m for m in words if len(x)==len(m)]
+print(l)
