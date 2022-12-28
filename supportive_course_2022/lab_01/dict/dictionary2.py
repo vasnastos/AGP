73964688599cfe538a50ea3bdb74ca9b045dfa23
NASTOS VASILEIOS <63415169+vasnastos@users.noreply.github.com>
2022-12-28 19:19:35 +0200

    Delete supportive_course_2022 directory

--- supportive_course_2022/lab_01/dict/dictionary2.py
+++
@@ -1,4 +0,0 @@
-# dictionary comprehension
-
-a_dict = {x:x*2 for x in [1,3,5,7,9]}
-print(a_dict)
--- supportive_course_2022/lab_01/tuple/tuple2.py
+++
@@ -1,3 +0,0 @@
-# tuple comprehension
-a_tuple = tuple(x * 2 for x in [1, 2, 3, 4, 5])
-print(a_tuple)
